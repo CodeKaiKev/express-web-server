44decff79f8ce91668af8adcc583aaf72f342f95
CodeKaiKev <CodeKaiKev@gmail.com>
2023-02-20 22:58:03 +0000

    Hi

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
 });
